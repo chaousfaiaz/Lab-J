8002299f8f793ed78deb71779f53164efbc7633c
chaousfaiaz <53284875+chaousfaiaz@users.noreply.github.com>
2025-04-09 10:03:38 -0400

    alignChannels.m

--- labJ/labJ/alignChannels.m
+++ labJ/labJ/alignChannels.m
@@ -14,5 +14,18 @@
 %
 %
 function imageResult = alignChannels(r, g, b)
-    %write your code here!
+    % write your code here!
+    target = r;
+    c1 = g;  g for green
+    c2 = b; b for blue
+    % function to compute ssd
+    % This measures the difference between two images
+    function score = ssd((chanel1 - chanel2).^2, 'all');
+    end
+    % Function to find the best allignment using SSD
+    function shift = findBestAllignment(channel, target)
+    minScore = inf;
+    bestShift = [0,0]; %[row_shift, col_shift]
+    
+    
 end
